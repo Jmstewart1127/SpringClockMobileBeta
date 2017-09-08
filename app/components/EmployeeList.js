4db7c513f5954470d79ba4880622a66837e25260
Jacob Stewart <jmstewart1127@gmail.com>
2017-09-07 19:46:09 -0500

    rest very close to working

--- app/components/EmployeeList.js
+++ app/components/EmployeeList.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component }                        from 'react';
 import { ActivityIndicator, ListView, Text, View } from 'react-native';
 
 class EmployeeList extends Component {
-  state = { clock: [] };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
 
   componentWillMount() {
     fetch('https://spring-clock.herokuapp.com/rest/employees')
-     .then((response) => response.json())
-     .then((responseData) => this.setState({ clock: responseData }));
-  }
-
-  renderEmployee() {
-    return this.state.clock.map(clock =>
-      <Text key={
-        clock.id
-      } clock={clock} />
-    );
+      .then((response) => response.json())
+      .then(responseJson => console.log(responseJson))
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View>
-        {this.renderEmployee()}
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}, {rowData}</Text>}
+      />
       </View>
     );
   }
