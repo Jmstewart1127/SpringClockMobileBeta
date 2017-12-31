7c9f5355d2a74f8d270e29180c863fcc645f3bbf
Jmstewart1127 <Jmstewart1127@gmail.com>
2017-12-31 15:42:35 -0600

    new

--- app/components/Jobs.js
+++ app/components/Jobs.js
@@ -7,7 +7,7 @@ class Jobs extends Component {
     super(props);
 
     this.state = {
-      isLoading: true,  
+      isLoading: true,
       jobs: [],
     };
   }
@@ -17,10 +17,16 @@ class Jobs extends Component {
     fetch('https://spring-clock.herokuapp.com/rest/jobs/assigned/employee/' + id)
     .then((response) => response.json())
     .then((responseJson) => {
+      console.log(responseJson);
+      var x = [];
+      for (let i=0; i<responseJson.length; i++) {
+        x.push(responseJson[i]);
+      }
       let ds = new ListView.DataSource({rowHasChanged: (r1, r2, r3, r4, r5, r6) => r1 !== r2});
       this.setState({
         isLoading: false,
         dataSource: ds.cloneWithRows(responseJson),
+        jobs: x,
       });
     })
     .catch((error) => {
@@ -33,15 +39,7 @@ class Jobs extends Component {
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          
-        </View>
-      );
-    }
-    
-    if (this.state.dataSource = []) {
+    if (this.state.isLoading || this.state.jobs.length < 1) {
       return (
         <View>
           <Text></Text>
@@ -54,11 +52,11 @@ class Jobs extends Component {
             {"Today's Job Site"}
           </Text>
           <ListView
-            dataSource={this.state.dataSource}
+            dataSource={ this.state.dataSource }
               renderRow={(rowData) =>
               <Text style={ styles.textStyle }>
                   <Text style={ styles.textStyle } >
-                  {rowData.jobAddress}
+                  { rowData.jobAddress }
                   {/* {"Week Time: " + rowData.weekTimeInHours},
                   {"Pay Rate: " + rowData.payRate},
                   {"Period Pay: " + rowData.totalPay},
@@ -74,46 +72,51 @@ class Jobs extends Component {
 }
 
 const styles = {
-    listStyle: {
-        width: 400,
-        borderRadius: 4,
-        borderWidth: 1,
-        borderColor: 'transparent',
-        backgroundColor: '#5C77E6',
-        padding: 10,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 0 },
-        shadowRadius: 0,
-        elevation: 15,
-      },
-    
-      userStyle: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginTop: 'auto',
-        marginBottom: 'auto',
-        color: 'white',
-        textAlign: 'left',
-        borderColor: 'white',
-        borderBottomWidth: 1,
-        height: 50,
-      },
-    
-      textStyle: {
-        color: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-      },
-    
-      buttonStyle: {
-        borderWidth: 1,
-        borderColor: 'blue',
-        borderStyle: 'solid',
-        borderRadius: 10,
-        backgroundColor: 'transparent',
-      }
+  viewStyle: {
+    marginLeft: 5,
+    marginRight: 5,
+  },
+
+  listStyle: {
+      borderRadius: 4,
+      borderWidth: 1,
+      borderColor: 'transparent',
+      backgroundColor: '#5C77E6',
+      padding: 10,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 0 },
+      shadowRadius: 0,
+      elevation: 15,
+      height: 100,
+      marginLeft: 5,
+      marginRight: 5,
+    },
+
+    userStyle: {
+      fontWeight: 'bold',
+      fontSize: 18,
+      marginTop: 'auto',
+      marginBottom: 'auto',
+      color: 'white',
+      textAlign: 'left',
+      borderColor: 'white',
+      borderBottomWidth: 1,
+      height: 50,
+    },
+
+    textStyle: {
+      color: 'white',
+      marginTop: 5,
+      marginBottom: 5,
+    },
+
+    buttonStyle: {
+      borderWidth: 1,
+      borderColor: 'blue',
+      borderStyle: 'solid',
+      borderRadius: 10,
+      backgroundColor: 'transparent',
     }
+  }
 
 export default Jobs;
