8ca4f797ff1e4304332725a989372d5eeea97af2
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-04-04 18:50:53 -0500

    refactored time clock algorithm

--- app/components/Location.js
+++ app/components/Location.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, AsyncStorage } from 'react-native';
-import { Avatar, Card, List, ListItem, Button, Divider } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, AsyncStorage } from 'react-native';
+import { List, ListItem, Button } from 'react-native-elements'
 
 class Location extends Component {
   constructor(props) {
@@ -13,8 +12,9 @@ class Location extends Component {
       addressLat: null,
       addressLng: null,
       coordinateMatches: [],
+      jobs: [],
       jobId: null,
-      clockStatus: null,
+      clockStatus: this.props.clockStatus,
       clockable: false,
       error: null,
     };
@@ -39,96 +39,59 @@ class Location extends Component {
   async _clockIn() {
     let id = await AsyncStorage.getItem('userId');
     let jobId = this.state.jobId;
-    console.log("this.state.jobId: " + jobId);
-    if (this.state.latitude && this.state.longitude) {
-      fetch('https://spring-clock.herokuapp.com/rest/mobile/clock/in/' + id + '/' + jobId)
-        .then((responseJson) => {
-          return responseJson;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
+    fetch('https://spring-clock.herokuapp.com/rest/mobile/clock/in/' + id + '/' + jobId)
+      .then((responseJson) => {
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   async _clockOut() {
     let id = await AsyncStorage.getItem('userId');
     let jobId = this.state.jobId;
-    console.log(jobId);
-    if (this.state.latitude && this.state.longitude) {
-      fetch('https://spring-clock.herokuapp.com/rest/mobile/clock/out/' + id + '/' + jobId)
-        .then((responseJson) => {
-          return responseJson;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }
-
-  _checkTrue(arr, val) {
-    return arr.some(function(arrVal) {
-      return val === arrVal;
-    });
-  }
-
-  _clockInAndOut(arr) {
-    if (this._checkTrue(arr, true)) {
-      this.setState({
-        clockStatus: true,
-        clockable: true,
+    fetch('https://spring-clock.herokuapp.com/rest/mobile/clock/out/' + id + '/' + jobId)
+      .then((responseJson) => {
+        return responseJson;
       })
-    } else {
-      this._clockOut();
-      this.setState({
-        clockStatus: false,
-      })
-    }
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
-  async _getAddresses() {
+  async _getAddressesTest() {
     let userId = await AsyncStorage.getItem('userId');
     fetch('https://spring-clock.herokuapp.com/rest/mobile/jobs/assigned/employee/' + userId)
       .then((response) => response.json())
       .then((responseJson) => {
-        const addresses = [];
-        const coordMatch = [];
-        const jobIds = [];
-        let jobId;
-        console.log(responseJson);
-        for (var i=0; i<responseJson.length; i++) {
-          console.log("id: " + responseJson[i].id);
-          console.log("ja: " + responseJson[i].jobAddress);
-          jobIds.push(responseJson[i].id);
-          addresses.push(responseJson[i].jobAddress);
-          for (var j=0; j<addresses.length; j++) {
-            let jobIdMatch = jobIds[j];
-            console.log("jobIds array: " + jobIds[j]);
-            console.log("addresses array: " + addresses[j]);
-            fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+ addresses[j].split(" ")
-              + '&key=AIzaSyDlXAOpZfmgDvrk4G7MkD6NXxPf9yJeJo8')
-              .then((response) => response.json())
-              .then((responseJson) => {
-                let lat = responseJson.results["0"].geometry.location.lat;
-                let lng = responseJson.results["0"].geometry.location.lng;
-                if (this._latMatch(this.state.latitude, lat) && this._lngMatch(this.state.longitude, lng)) {
-                  coordMatch.push(true);
-                  jobId = jobIdMatch;
-                  console.log("jobId: " + jobId);
-                } else {
-                  coordMatch.push(false);
-                }
-                this.setState({
-                  coordinateMatches: coordMatch,
-                  jobId: jobId,
-                });
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-          }
-        }
+        this.setState({
+          jobs: responseJson
+        });
+      })
+      .catch((error) => {
+        console.error(error);
       });
+  }
+
+  _compareLocationToPremises() {
+    const coordMatch = [];
+    let jobId;
+    for (let i=0; i<this.state.jobs.length; i++) {
+      let lat = this.state.jobs[i].latitude;
+      let lng = this.state.jobs[i].longitude;
+      if (this._latMatch(this.state.latitude, lat)
+        && this._lngMatch(this.state.longitude, lng)) {
+        coordMatch.push(true);
+        jobId = this.state.jobs[i].id;
+      } else {
+        coordMatch.push(false);
+      }
+    }
+    this.setState({
+      coordinateMatches: coordMatch,
+      jobId: jobId,
+    });
   }
 
   _getCurrentLocation() {
@@ -139,14 +102,32 @@ class Location extends Component {
           longitude: position.coords.longitude,
           error: null,
         });
-        this._getAddresses();
+        this._compareLocationToPremises();
         this._clockInAndOut(this.state.coordinateMatches);
-        console.log("looking");
-        console.log("state.jobId: " + this.state.jobId);
       },
       (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
+  }
+
+  _checkTrue(arr, val) {
+    return arr.some(function(arrVal) {
+      return val === arrVal;
+    });
+  }
+
+  _clockInAndOut(arr) {
+    if (this._checkTrue(arr, true)) {
+      this._clockIn();
+      this.setState({
+        clockStatus: true,
+        clockable: true,
+      })
+    } else {
+      this.setState({
+        clockStatus: false,
+      })
+    }
   }
 
   _manualClockInOut() {
@@ -155,28 +136,21 @@ class Location extends Component {
       this.setState({
         clockStatus: false,
       });
-      console.log(this.state.clockStatus);
     } else {
       this._getCurrentLocation();
-      if (this.state.clockable) {
-        this._clockIn();
-      }
     }
   }
 
   _clockStatusText() {
-    if (this.state.clockStatus === null) {
-      if (this.props.clockStatus) {
-        return "Clocked In"
-      } else {
-        return "Clocked Out"
-      }
-    }
     if (this.state.clockStatus) {
       return "Clocked In";
     } else {
       return "Clocked Out";
     }
+  }
+
+  componentDidMount() {
+    this._getAddressesTest();
   }
 
   render() {
